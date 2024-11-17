d785c5dd4590c81b1d0b25c2e1dad0dbe7b63a27
virusman <virusman@virusman.ru>
2024-11-17 14:41:18 -0800

    Remove Crashlytics from the sample app

--- sample/src/main/java/com/github/lukaspili/reactivebilling/sample/App.java
+++ sample/src/main/java/com/github/lukaspili/reactivebilling/sample/App.java
@@ -2,10 +2,7 @@ package com.github.lukaspili.reactivebilling.sample;
 
 import android.app.Application;
 
-import com.crashlytics.android.Crashlytics;
 import com.github.lukaspili.reactivebilling.ReactiveBilling;
-
-import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by lukasz on 08/05/16.
@@ -15,11 +12,6 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
-        //noinspection PointlessBooleanExpression
-        if (!BuildConfig.DEBUG) {
-            Fabric.with(this, new Crashlytics());
-        }
 
         ReactiveBilling.initLogger(true);
     }
